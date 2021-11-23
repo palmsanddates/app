e54f3bea42e11c57bb741cfbe3bc3553e479c617
shahrour <ahmadshahrour951@gmail.com>
2021-11-22 21:22:27 -0800

    Update API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,17 @@
-import axios from 'axios'
+import axios from 'axios';
+
+let baseURL;
+
+switch (process.env.NODE_ENV) {
+  case 'production':
+    baseURL = 'https://api.palmsanddates.com';
+    break;
+  default:
+    baseURL = 'http://localhost:5000';
+    break;
+}
 
 export default axios.create({
-  baseURL: 'https://api.palmsanddates.com',
-  responseType: 'json'
-})
+  baseURL,
+  responseType: 'json',
+});
